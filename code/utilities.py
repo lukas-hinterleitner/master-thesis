0f422a20b94e3b1e3d4d763b8e147d31a9f270fc
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2024-09-06 09:09:16 +0200

    add gradient operations to get gradients based on samples

--- code/utilities.py
+++ code/utilities.py
@@ -1,7 +1,19 @@
 from functools import partial
 
+import transformers.models.auto.tokenization_auto
 from datasets import DatasetDict, Dataset, IterableDatasetDict, IterableDataset
-from transformers import PreTrainedTokenizerFast
+from torch.utils.data import DataLoader
+from transformers import PreTrainedTokenizerFast, DataCollatorForSeq2Seq, PreTrainedModel, PreTrainedTokenizerBase
+
+from open_instruct.finetune import encode_with_messages_format
+
+
+def print_red(skk):
+    print("\033[91m {}\033[00m".format(skk))
+
+
+def print_green(skk):
+    print("\033[92m {}\033[00m".format(skk))
 
 
 def __preprocess(example, tokenizer: PreTrainedTokenizerFast, max_seq_len: int):
@@ -66,3 +78,36 @@ def transform_dataset(dataset: DatasetDict | Dataset | IterableDatasetDict | Ite
     print(f"Total tokens: {total_tokens:,d}")
 
     return dataset
+
+
+def prepare_dataset(dataset: [Dataset | DatasetDict], model: PreTrainedModel, tokenizer: PreTrainedTokenizerBase) -> DataLoader:
+    encode_function = partial(
+        encode_with_messages_format,
+        tokenizer=tokenizer,
+        max_seq_length=2048,
+        add_bos=False,
+    )
+
+    lm_dataset = dataset.map(
+        encode_function,
+        batched=False,
+        num_proc=1,
+        load_from_cache_file=False,
+        remove_columns=[
+            name
+            for name in dataset.column_names
+            if name not in ["input_ids", "labels", "attention_mask"]
+        ],
+        desc="Tokenizing and reformatting instruction data",
+    )
+    lm_dataset.set_format(type="pt")
+    lm_dataset = lm_dataset.filter(lambda example: (example["labels"] != -100).any())
+
+    train_dataloader = DataLoader(
+        lm_dataset,
+        shuffle=False,
+        collate_fn=DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model, padding="longest"),
+        batch_size=1,
+    )
+
+    return train_dataloader
