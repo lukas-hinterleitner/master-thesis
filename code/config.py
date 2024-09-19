f37c9b883137cb4b625f00e162858423d185b489
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2024-09-19 09:20:45 +0200

    add paraphrased data to repository

--- code/config.py
+++ code/config.py
@@ -6,7 +6,7 @@ data_folder_path = "../data"
 
 lima_dataset_path = os.path.join(data_folder_path, "lima")
 lima_filtered_dataset_path = os.path.join(lima_dataset_path, "filtered")
-lima_filtered_paraphrased_dataset_path = os.path.join(lima_filtered_dataset_path, "paraphrased")
+lima_filtered_paraphrased_dataset_path = os.path.join(data_folder_path, "paraphrased")
 
 if not os.path.exists(lima_filtered_paraphrased_dataset_path):
     os.makedirs(lima_filtered_paraphrased_dataset_path)
