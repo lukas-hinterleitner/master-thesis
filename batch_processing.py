997bb36b096eb62189f4aebfac30d8debdb3c73b
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2025-06-24 21:19:45 +0200

    update batch processing to only use nodes with bigger GPU

--- batch_processing.py
+++ batch_processing.py
@@ -72,6 +72,7 @@ def create_slurm_script(partition_idx, start_idx, end_idx, script_dir, setting, 
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --time=1-12:00:00
+#SBATCH --nodelist=dgx-h100-em2
 #SBATCH --output=./out/{job_name}/slurm-%j.out
 #SBATCH --job-name={job_name}
 
