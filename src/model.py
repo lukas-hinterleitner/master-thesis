7f2e6face0bb8d3943bbdf587fe06a3426990aea
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2025-07-14 19:52:21 +0200

    improve performance when loading model into ram/vram

--- src/model.py
+++ src/model.py
@@ -6,11 +6,10 @@ from src.config.dataset import get_chat_template
 
 
 def get_model(model_name = MODEL_NAME, device = DEVICE) -> PreTrainedModel:
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device)
 
-    print(f"Device: {device}")
+    print(f"Device: {model.device}")
 
-    model.to(device)
     model.eval()  # set to evaluation because we don't need to update weights
 
     print(f"Model parameters: {model.num_parameters()}")
